99f0b63fb32bca3c232aaa7b6a141a03eca0207d
Max Mecklin <max@meckl.in>
2021-03-02 15:19:20 +0200

    UUID Json serialize

--- backend/core/database/__init__.py
+++ backend/core/database/__init__.py
@@ -1,4 +1,6 @@
 import asyncio
+from json import JSONEncoder
+from uuid import UUID
 
 from config import settings
 from sqlalchemy import create_engine
@@ -13,3 +15,15 @@ Base = declarative_base()
 Base.query = Session.query_property()
 
 session_lock = asyncio.Lock()
+
+# UUID JSON encoding hack
+JSONEncoder_olddefault = JSONEncoder.default
+
+
+def JSONEncoder_newdefault(self, o):
+    if isinstance(o, UUID):
+        return str(o)
+    return JSONEncoder_olddefault(self, o)
+
+
+JSONEncoder.default = JSONEncoder_newdefault
