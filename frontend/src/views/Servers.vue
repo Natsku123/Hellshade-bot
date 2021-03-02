a509bcce8c64676912f9536a038c2feb34d9856d
Max Mecklin <max@meckl.in>
2021-03-02 14:18:11 +0200

    Get servers and UUID fixes

--- frontend/src/views/Servers.vue
+++ frontend/src/views/Servers.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="Servers">
+    <h1>Servers</h1>
+    <router-link v-for="server in servers" :key="server.node.uuid" :to="'/servers/' + server.node.uuid">{{server.node.name}}</router-link>
   </div>
 </template>
 
 <script>
+import { client } from '@/utils';
+import { gql } from 'graphql-request';
 export default {
   name: 'Servers',
   components: {
 
+  },
+  data: () => ({
+    servers: [],
+  }),
+  mounted() {
+    const query = gql`
+        {
+            allServers {
+                edges {
+                    node {
+                        uuid
+                        name
+                    }
+                }
+            }
+        }
+    `
+    client.request(query).then(
+        (data) => {
+          data['allServers']['edges'].forEach((n) => {
+            n.node.uuid = n.node.uuid.substring(1, n.node.uuid.length-1);
+          });
+          this.servers = data['allServers']['edges'];
+        }
+    );
   }
 }
 </script>
