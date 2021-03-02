810a76b5b29e5fd5305d52efb29a0570d63cf900
Max Mecklin <max@meckl.in>
2021-03-02 16:44:35 +0200

    MemberCard component and listing members on server page

--- frontend/src/views/Server.vue
+++ frontend/src/views/Server.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="server">
-    <h1> {{ server.name }}</h1>
-    <p v-for="member in members" :key="member.uuid">{{ member }}</p>
+    <v-row justify="center"><v-col><h1>{{ server.name }}</h1></v-col></v-row>
+    <v-row justify="center"><v-col><h2>Top 10</h2></v-col></v-row>
+    <v-row>
+      <v-col md="3" v-for="member in top10" :key="member.uuid" class="">
+        <MemberCard :member="member" />
+      </v-col>
+    </v-row>
+    <v-row justify="center"><v-col><h2>All Members</h2></v-col></v-row>
+    <v-row>
+      <v-col md="3" v-for="member in members" :key="member.uuid" class="">
+        <MemberCard :member="member" />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import { client } from "@/utils";
 import {gql} from "graphql-request";
+import MemberCard from "@/components/MemberCard";
 
 export default {
   name: "Server",
+  components: {MemberCard},
   data: () => ({
     server: {
       name: "Loading...",
       uuid: ""
     },
     members: [],
+    top10: [],
     loading: true
   }),
   methods: {
@@ -43,7 +57,6 @@ export default {
                 level {
                     uuid
                     value
-                    exp
                 }
                 exp
             }
@@ -51,8 +64,8 @@ export default {
       `
 
       client.request(serverQuery).then((data) => {
-        data['server'][0].uuid = data['server'].uuid.substring(1, data['server'].uuid.length-1);
-        this.server = data['server'];
+        data['server'][0].uuid = data['server'][0].uuid.substring(1, data['server'][0].uuid.length-1);
+        this.server = data['server'][0];
         this.loading = false;
       }).catch(() => {
         this.loading = false;
@@ -62,7 +75,35 @@ export default {
         data.member.forEach((member) => {
           member.uuid = member.uuid.substring(1, member.uuid.length-1);
         });
-        this.members = data.member.filter(member => !member.player.hidden);
+        this.members = data.member.filter(member => !member.player.hidden && member.player.name !== "UNKNOWN");
+        this.members.sort((a, b) => {
+          if (a.player.name < b.player.name) {
+            return -1;
+          } else if (a.player.name > b.player.name) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        this.top10 = data.member.sort((a, b) => {
+          if (a.level === null && b.level !== null) {
+            return 1;
+          } else if (a.level !== null && b.level === null) {
+            return -1;
+          } else if (a.level === null && b.level === null) {
+            return 0;
+          } else if (a.level.value < b.level.value) {
+            return 1;
+          } else if (a.level.value > b.level.value) {
+            return -1;
+          } else if (a.exp < b.exp) {
+            return 1;
+          } else if (a.exp > b.exp) {
+            return -1;
+          } else {
+            return 0;
+          }
+        }).slice(0, 10);
       })
     }
   },
