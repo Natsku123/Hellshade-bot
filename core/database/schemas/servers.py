05fe7f233ea6b388b4d02dc6a08f562a03f40083
Max Mecklin <max@meckl.in>
2021-03-10 11:56:53 +0200

    Add roles channel and message

--- core/database/schemas/servers.py
+++ core/database/schemas/servers.py
@@ -7,6 +7,8 @@ class ServerBase(BaseModel):
     name: str
     server_exp: int
     channel: Optional[str]
+    role_channel: Optional[str]
+    role_message: Optional[str]
 
     class Config:
         orm_mode = True
@@ -20,3 +22,5 @@ class UpdateServer(BaseModel):
     name: Optional[str]
     server_exp: Optional[int]
     channel: Optional[str]
+    role_channel: Optional[str]
+    role_message: Optional[str]
