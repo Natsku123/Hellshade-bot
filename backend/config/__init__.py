c8e75113ff6f0386c9d929b4eceb393ee2a7efb9
Max Mecklin <max@meckl.in>
2021-04-30 13:56:24 +0300

    Settings fixes

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings, AnyHttpUrl
 class Settings(BaseSettings):
     SERVER_NAME: str = os.environ.get("SERVER_NAME", "Hellshade-bot")
     DATABASE_URL: str = os.environ.get("DB_URL", f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@db/{os.environ.get('DB_NAME')}")
-    ORIGINS: List[AnyHttpUrl] = os.environ.get("ORIGINS", ["http://localhost:3080", "http://localhost:8080"])
+    ORIGINS: List[AnyHttpUrl] = os.environ.get("ORIGINS", ["http://localhost:3080", "http://localhost:8080", os.environ.get('SITE_URL')])
 
     class Config:
         case_sensitive = True
